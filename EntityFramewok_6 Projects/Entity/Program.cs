ebc774a47002a2de14221d86d5774314c0b4b406
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-05-18 15:19:59 +0400

    DataBase First --> simple example

---
+++ EntityFramewok_6 Projects/Entity/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TableContext();
+           //AddingIntoTable().Wait();
+        }
+
+        private static void TableContext()
+        {
+            BetADOEntities context = new BetADOEntities();
+           List<Student> list = context.Students.ToList();
+            foreach (Student item in list)
+            {
+                Console.WriteLine($"{item.Id}, {item.FirstName}, {item.LastName}, {item.TimeStamp} ");
+            }
+        }
+
+        private static async Task AddingIntoTable()
+        {
+            BetADOEntities context = new BetADOEntities();
+            context.Students.Add(new Student{FirstName = "Poghas", LastName = "Petros"});
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.InnerException.Message);
+            }
+        }
+    }
+}
